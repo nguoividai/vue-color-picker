2cc4fe0a4ae1b85fd7316a878c3a874bb83cde70
nguoividai <nguoividai@outlook.com.vn>
2023-07-01 15:28:45 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <ColorPanel />
+  </div>
+</template>
+
+<script>
+import ColorPanel from "./components/ColorPanel";
+
+export default {
+  name: "App",
+  components: { ColorPanel },
+  mounted() {},
+};
+</script>
+
+
